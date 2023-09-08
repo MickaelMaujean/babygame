55d01dab76c9e80188f2d418cdeb5082ab4e451a
MickaelMaujean <mickael.maujean@hotmail.fr>
2023-09-08 08:24:29 +0200

    Complete env. variable for frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .routers import votes
 from .database import engine
+from .config import settings
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -10,7 +11,7 @@ app = FastAPI()
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with your frontend's URL
+    allow_origins=[settings.frontend_localhost_url],  # Replace with your frontend's URL
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
