e4144ffb0001031e647f86c481c7939b2903d2bd
MickaelMaujean <mickael.maujean@hotmail.fr>
2023-09-26 14:56:22 +0200

    deploy with lofoten

--- frontend/src/components/Section1.js
+++ frontend/src/components/Section1.js
@@ -43,10 +43,10 @@ function Section1({ content }) {
   return (
     <div className="section1">
       <h2> &#x1F44B; Bienvenue {userData && ` ${userData.email}`} &#x1F44B;</h2>
-      <h1>Voici quelques informations concernant les règles du jeu et le fonctionnement &#x1F476; :</h1>
+      <h1>Voici quelques informations concernant les règles du jeu et son fonctionnement &#x1F476; :</h1>
       <ul>
       <li>Tout d'abord indiques ton prénom et ton nom afin de te retrouver en cas de victoire &#x1F604;</li>
-      <li>Pour le sexe c'est Boy ou Girl &#x1F604;</li>
+      <li>Pour le sexe c'est "Boy" ou "Girl" &#x1F604;</li>
       <li>La taille (en cm) &#x1F4CF;</li>
       <li>Le poids (en kg) &#x2696;</li>
       <li>Et enfin la date de naissance, tu dois également deviner l'heure !&#x23F0;</li>
